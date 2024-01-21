db45847e03e1ee0913107f80753e1408b4022aa1
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2024-01-21 21:04:09 +0530

    added logic to POST the product list of the authorized user

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const sqldb = require("./db/conn");
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const verifyToken = require("./middleware/verifyToken");
 
 const port = process.env.PORT || 8000;
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
     try {
@@ -120,7 +122,7 @@ app.post("/login", async (req, res) => {
 app.get("/products", verifyToken, (req, res) => {
     const uid = req.uid;
 
-    const fetchDataQuery = "SELECT * FROM products WHERE uid = ?";
+    const fetchDataQuery = "SELECT * FROM products WHERE uid = ? AND isactive = 'y'";
     const value = [uid];
     sqldb.query(fetchDataQuery, value, (err, results) => {
         if (err) {
@@ -131,8 +133,27 @@ app.get("/products", verifyToken, (req, res) => {
     });
 });
 
-// app.post("/products", (req, res) => {});
+app.post("/products", verifyToken, (req, res) => {
+    const uid = req.uid;
+    const product = req.body.product;
+
+    if (!product) {
+        return res.status(400).json({ error: "Product is required in the request body" });
+    }
+
+    const insertProductQuery = "INSERT INTO products (uid, product) VALUES (?, ?)";
+    const values = [uid, product];
+
+    sqldb.query(insertProductQuery, values, (err, results) => {
+        if (err) {
+            console.error("Error executing MySQL query:", err);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        res.json({ message: "Product added successfully" });
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
