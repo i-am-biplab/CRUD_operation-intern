52e2bf1a5a66cbb3f1e3a65d72f8cc176b190842
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2024-01-22 12:17:23 +0530

    add url of backend login

--- frontend/crudopeapp/src/Login.js
+++ frontend/crudopeapp/src/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     try {
       // Make an API call to authenticate the user
-      const response = await fetch('/login', {
+      const response = await fetch('http://127.0.0.1:8000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
